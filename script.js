b08adb36c6c5d26e1669e69eead2a972b556d46d
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2020-12-17 16:06:57 +0000

    I know its not doen but class is

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.getElementById("btn").addEventListener("click", myFunction);
+document.getElementById('btn').addEventListener('click', myFunction);
 function myFunction() {
-  var x = document.getElementById("box").value
+  var x = document.getElementById('box').value
   x = parseInt(x);
   if (x > 0) {
-    alert("that is a positive number")
+    alert('that is a positive number')
   } else if (x < 0) {
-    alert("that is a negative number")
+    alert('that is a negative number')
   } else {
-    alert("that isn't a positive or a negative number")
+    alert('that isnt a positive or a negative number')
   }
 }
